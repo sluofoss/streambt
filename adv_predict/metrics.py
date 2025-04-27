2 return pct metrics and found bug, also should consider whether the precision should be changed.

adv_predict/metrics.py
@@ -9,6 +9,11 @@ def precision_at(y_true:NDArray[np.float64], y_pred:NDArray[np.float64], thresho
     """
     choice of threshold depends on the regression dataset
     """
+    # TODO:
+    # should this be changed?
+    # e.g. when  predict > 1.x and true > 1, should this still technically be considered a win?
+    # i.e. should there be 2 thresholds? 1 for predict and 1 for true. 
+    #
     y_t_alt = (y_true > threshold)
     y_p_alt = (y_pred > threshold)
     #print('---------------------')
@@ -29,6 +34,64 @@ def true_positive_at(y_true:NDArray[np.float64], y_pred:NDArray[np.float64], thr
     tn, fp, fn, tp = cm.ravel()
     return float(tp)
 
+
+def mthly_rtrn_pct_avg(
+    y_true:NDArray[np.float64], 
+    y_pred:NDArray[np.float64], 
+    threshold:float, 
+    date:NDArray[np.datetime64]
+)->float:
+    y_t_alt = (y_true > threshold)
+    y_p_alt = (y_pred > threshold)
+    #print(np.stack([date, y_t_alt, y_p_alt],axis=1, dtype=object).shape)
+    #df = pl.from_numpy(np.stack([date, y_t_alt, y_p_alt],axis=1, dtype=object), schema=[("date",pl.Datetime), ("true",pl.Boolean), ('predict',pl.Boolean)], orient="row")
+    df = pl.DataFrame({
+        "date": date.astype('datetime64[ms]'),  # Ensure datetime format
+        "true": y_t_alt.astype(bool),
+        "predict": y_p_alt.astype(bool),
+        'true_pct': y_true,
+    })
+    #print(df)
+    agg = df.filter(pl.col('predict')==True).group_by(pl.col('date').dt.month_start()).agg( 
+        pl.col('true_pct').mean()
+    )
+    #print(agg)
+    #print(agg.mean())
+    res = agg.select('true_pct').mean().item()
+    if agg['true_pct'].len() == 0:
+        return 1
+    if res is None:
+        print('debug pct avg:')
+        print(agg, res)
+    return res
+
+def mthly_rtrn_pct_std(
+    y_true:NDArray[np.float64], 
+    y_pred:NDArray[np.float64], 
+    threshold:float, 
+    date:NDArray[np.datetime64]
+)->float:
+    y_t_alt = (y_true > threshold)
+    y_p_alt = (y_pred > threshold)
+    #print(np.stack([date, y_t_alt, y_p_alt],axis=1, dtype=object).shape)
+    #df = pl.from_numpy(np.stack([date, y_t_alt, y_p_alt],axis=1, dtype=object), schema=[("date",pl.Datetime), ("true",pl.Boolean), ('predict',pl.Boolean)], orient="row")
+    df = pl.DataFrame({
+        "date": date.astype('datetime64[ms]'),  # Ensure datetime format
+        "true": y_t_alt.astype(bool),
+        "predict": y_p_alt.astype(bool),
+        'true_pct': y_true,
+    })
+    #print(df)
+    agg = df.filter(pl.col('predict')==True).group_by(pl.col('date').dt.month_start()).agg( 
+        pl.col('true_pct').mean()
+    )
+    res = agg.select('true_pct').std().item()
+    if agg['true_pct'].len() == 0:
+        return 0
+    if res is None:
+        print('debug pct std:')
+        print(agg, res)
+    return res
 
 def monthly_win_ratio_avg(
     y_true:NDArray[np.float64], 
@@ -55,7 +118,11 @@ def monthly_win_ratio_avg(
     #print(agg)
     if agg['TP'].len() == 0: # tp should be relabeled as precision here
         return 0
-    return agg.mean().item()
+    res = agg.mean().item()
+    if res is None:
+        print('debug win avg:')
+        print(agg, res)
+    return res
 
 def monthly_win_ratio_std(
     y_true:NDArray[np.float64], 
@@ -74,15 +141,37 @@ def monthly_win_ratio_std(
         ((pl.col('true')==pl.col('predict')) & (pl.col('true')==True)).cast(pl.Int32).sum().alias('TP'), 
         ((pl.col('true')!=pl.col('predict')) & (pl.col('true')==False)).cast(pl.Int32).sum().alias('FP'), 
     ).select(pl.col('TP')/(pl.col('TP')+pl.col('FP'))).drop_nans()
+    # TODO URGENT!!!:
+    # this metric and probably the others as well breaks with following output
+    """
+      _warn_prf(average, modifier, f"{metric.capitalize()} is", len(result))
+debug win std:
+shape: (1, 1)
+┌─────┐
+│ TP  │
+│ --- │
+│ f64 │
+╞═════╡
+│ 1.0 │
+└─────┘ None
+monthly_win_ratio_std threshold_1.01 None"""
+    # need to break the above agg step into multiple steps and inspect details 
+    
     #print(agg)
     if agg['TP'].len() == 0: # tp should be relabeled as precision here
         return 0
-    return agg.std().item()
+    res = agg.std().item()
+    if res is None:
+        print('debug win std:')
+        print(agg, res)
+    return res
 
 import functools as ft
 def named_partial(func, *args, **kwargs):
     f = ft.partial(func, *args, **kwargs) 
     #f.__name__ = f"{f.func.__name__}({",".join(list(args)+[f"{k}={v}" for k,v in kwargs.items()])})"
-    f.__name__ = f"{f.func.__name__}-{",".join(list(args)+[f"{k}:{v}" for k,v in kwargs.items()])}"
+    f.__name__ = f"{f.func.__name__} {"__".join(list(args)+[f"{k}_{v}" for k,v in kwargs.items()])}"
+    # WARNING: for parameter 'name' supplied: Names may only contain alphanumerics, underscores (_), dashes (-), periods (.), spaces ( ), colon(:) and slashes (/).
+    
     # TODO decide whether it should include unfilled partial arguments here 
     return f

test/test_metrics.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import adv_predict
-from adv_predict.metrics import monthly_win_ratio_avg, monthly_win_ratio_std
+from adv_predict.metrics import monthly_win_ratio_avg, monthly_win_ratio_std, mthly_rtrn_pct_std, mthly_rtrn_pct_avg
 
 import numpy as np 
 d = np.array(['2001-01-01', '2001-01-02', '2001-02-01', '2001-02-02', '2001-03-01', '2001-03-02', '2001-04-01', '2001-04-02'], dtype='datetime64')
@@ -16,5 +16,12 @@ print(
 
 print(
     'std', monthly_win_ratio_std(t,p,1,d)
+)
 
+print(
+    'std', mthly_rtrn_pct_std(t,p,1,d)
+)
+
+print(
+    'std', mthly_rtrn_pct_avg(t,p,1,d)
 )